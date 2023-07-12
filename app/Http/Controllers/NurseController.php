9d65b873ffe1aa6cf1a91553a71d7de54faa60cd
puteridxyu <puteridxyu@gmail.com>
2023-07-12 15:32:03 +0800

    .

--- app/Http/Controllers/NurseController.php
+++ app/Http/Controllers/NurseController.php
@@ -7,5 +7,35 @@ use Illuminate\Http\Request;
 
 class NurseController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('nurse.index');
+    }
+
+    public function viewDoctorList()
+    {
+        return view('admin.contents.doctorList');
+    }
+
+    public function viewPatientList()
+    {
+        return view('admin.contents.patientList');
+    }
+
+    public function viewRoomList()
+    {
+        return view('admin.contents.roomList');
+    }
+
+    public function viewAppointmentList()
+    {
+        return view('admin.contents.appointmentList');
+    }
+
+    public function viewMedicineList()
+    {
+        return view('admin.contents.medicineList');
+    }
+
 }
+
--- resources/views/nurse/includes/sidebar.blade.php
+++ resources/views/nurse/includes/sidebar.blade.php
@@ -2,18 +2,17 @@
 <nav class="pcoded-navbar">
     <div class="nav-list">
         <div class="pcoded-inner-navbar main-menu">
-        <div class="pcoded-navigation-label">Navigation</div>
+            <div class="pcoded-navigation-label">Navigation</div>
             <ul class="pcoded-item pcoded-left-item">
                 <li class="">
-                    <a href="../admin/adminPage.php?p=dashboard" class="waves-effect waves-dark">
-                        <span class="pcoded-micon">
-                            <i class="feather icon-home"></i>
-                        </span>
+                    <a href="/nurse/dashboard" class="waves-effect waves-dark">
+                        <span class="pcoded-micon"><i class="feather icon-home"></i></span>
                         <span class="pcoded-mtext">Dashboard</span>
                     </a>
                 </li>
+
                 <li class="">
-                    <a href="../admin/adminPage.php?p=doctorList" class="waves-effect waves-dark">
+                    <a href="/nurse/doctorList" class="waves-effect waves-dark">
                         <span class="pcoded-micon">
                             <i class="fas fa-solid fa-stethoscope"></i>
                         </span>
@@ -22,37 +21,37 @@
                 </li>
 
                 <li class="">
-                    <a href="../admin/adminPage.php?p=nurseList" class="waves-effect waves-dark">
+                    <a href="/nurse/patientList" class="waves-effect waves-dark">
                         <span class="pcoded-micon">
-                        <i class="fas fa-solid fa-user-nurse"></i>
-                        </span>
-                        <span class="pcoded-mtext">Nurses</span>
-                    </a>
-                </li>
-
-                <li class="">
-                    <a href="../admin/adminPage.php?p=patientList" class="waves-effect waves-dark">
-                        <span class="pcoded-micon">
-                            <i class="fas fa-regular fa-hospital-user"></i>
-                        </span>
+                            <i class="fas fa-regular fa-hospital-user"></i></span>
                         <span class="pcoded-mtext">Patients</span>
                     </a>
                 </li>
 
                 <li class="">
-                    <a href="../admin/adminPage.php?p=roomsList" class="waves-effect waves-dark">
+                    <a href="/nurse/roomList" class="waves-effect waves-dark">
                         <span class="pcoded-micon">
-                            <i class="fas fa-solid fa-home"></i>
+                            <i class="fas fa-solid fa-stethoscope"></i>
                         </span>
                         <span class="pcoded-mtext">Rooms</span>
                     </a>
                 </li>
+
                 <li class="">
-                    <a href="../admin/adminPage.php?p=appointmentList" class="waves-effect waves-dark">
+                    <a href="/nurse/appointmentList" class="waves-effect waves-dark">
                         <span class="pcoded-micon">
-                            <i class="fas fa-solid fa-calendar-check"></i>
+                        <i class="fas fa-solid fa-calendar-check"></i>
                         </span>
                         <span class="pcoded-mtext">Appointments</span>
+                    </a>
+                </li>
+
+                <li class="">
+                    <a href="/nurse/medicineList" class="waves-effect waves-dark">
+                        <span class="pcoded-micon">
+                        <i class="fas fa-regular fa-capsules"></i>
+                        </span>
+                        <span class="pcoded-mtext">Medicine</span>
                     </a>
                 </li>
             </ul>
