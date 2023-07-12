33e00427461ccc37e1b9b941951aa818f129f204
Master <niazhar96@gmail.com>
2023-07-12 10:12:32 +0800

    ...

--- resources/views/pharma/content.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-function redirectPage($i){
-    include ('contents/'.$i . '.php');
-}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Auth::routes();
+Route::get('/', function () {
+    return view('admin.index');
+});
+//Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
