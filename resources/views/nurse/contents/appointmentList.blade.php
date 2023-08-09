3251b31d6d1a7bc995e86382cceb9d86f1d3794f
FatinSyahidah <fatinsyahidah75@gmail.com>
2023-08-09 14:40:01 +0800

    modal

--- resources/views/nurse/contents/appointmentList.blade.php
+++ resources/views/nurse/contents/appointmentList.blade.php
@@ -83,6 +83,9 @@
                                                     </tr>
                                                 @else
                                                     @foreach($appointments as $appointment)
+                                                        @php
+                                                            $currentDate = now()->format('Y-m-d');
+                                                        @endphp
                                                         <tr>
                                                             <td>{{ $loop->iteration }}</td>
                                                             <td>{{ $appointment->doctor_name }}</td>
@@ -106,7 +109,9 @@
                                                                 @elseif ($appointment->status == 2)
                                                                     <span class="status badge badge-danger mb-2 align-top" style="font-size: 11px;">Cancelled</span>
 
-                                                                
+                                                                @elseif ($appointment->date < $currentDate)
+                                                                    <span class="status badge badge-warning mb-2 align-top" style="font-size: 11px;">No Action</span>
+
                                                                 @else
                                                                     <a title="Edit Appointment" data-toggle="modal" data-target="#editModal-{{ $appointment->id }}">
                                                                         <i style="font-size:20px;" class="icon feather icon-edit f-w-600 f-16 m-r-15 text-c-green"></i>
