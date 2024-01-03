bd08591682fad3f6c36db000394f9853ad760753
aishahzbr <aishahzubir74@gmail.com>
2024-01-03 10:19:06 +0800

    Update dashboard.blade.php

--- resources/views/patient/contents/dashboard.blade.php
+++ resources/views/patient/contents/dashboard.blade.php
@@ -22,7 +22,7 @@
                   <div class="page-header-title">
                     <i class="feather icon-home bg-c-blue"></i>
                     <div class="d-inline">
-                      <h5>Welcome {{$name}}!</h5>
+                      <h5>Welcome, {{$name}}!</h5>
                       <span>Are you feeling great today?</span>
                     </div>
                   </div>
@@ -31,12 +31,12 @@
                   <div class="page-header-breadcrumb">
                     <ul class=" breadcrumb breadcrumb-title">
                       <li class="breadcrumb-item">
-                        <a href="index.php">
+                        <a href="/patient/dashboard">
                           <i class="feather icon-home"></i>
                         </a>
                       </li>
                       <li class="breadcrumb-item">
-                        <a href="../patient/index.php">Dashboard</a>
+                        <a href="/patient/dashboard">Dashboard</a>
                       </li>
                     </ul>
                   </div>
@@ -49,9 +49,55 @@
                 <div class="page-wrapper">
                   <div class="page-body">
                     <div class="row">
+                    <div class="col-md-4">
+                          <div class="card comp-card" style="height:140px">
+                            <div class="card-body">
+                              <div class="row align-items-center">
+                                <div class="col">
+                                  <h6 class="m-b-25"> Queue No.</h6>
+                                @if ($aptDs->isEmpty())
+                                    <h4 class="f-w-700 text-c-green">N/A</h4>
+                                @else
+                                    @foreach ($aptDs as $aptD)
+                                        <h4 class="f-w-700 text-c-green">{{ $aptD->queueid }}</h4>
+                                    @endforeach
+                                @endif
+
+                                </div>
+                                <div class="col-auto">
+                                  <i class="fas fa-user-friends bg-c-green" style="margin-top: 22px;"></i>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                      </div>
+
+                      <div class="col-md-4" >
+                          <div class="card comp-card">
+                            <div class="card-body">
+                              <div class="row align-items-center">
+                                <div class="col">
+                                  <h6 class="m-b-25">Room</h6>
+                                  @if ($aptDs->isEmpty())
+                                    <h4 class="f-w-700 text-c-green">N/A</h4>
+                                  @else
+                                  @foreach ($aptDs as $aptD)
+                                      <h4 class="f-w-700 text-c-yellow">{{ $aptD->room_name ?? 'N/A' }}</h4>
+
+                                  @endforeach
+                                  @endif
+                                </div>
+                                <div class="col-auto">
+                                  <i class="fas fa-door-open bg-c-yellow"></i>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                      </div>
+
                       <div class="col-md-4">
                         <a title="View Appointment" data-toggle="modal" data-target="#viewModal">
-                          <div class="card comp-card">
+                          <div class="card comp-card" style="height:140px">
                             <div class="card-body">
                               <div class="row align-items-center">
                                 <div class="col">
@@ -67,7 +113,7 @@
                         </a>
                       </div>
 
-                      <div class="col-md-4">
+                      <div class="col-md-4" hidden>
                         <div class="card comp-card">
                           <div class="card-body">
                             <div class="row align-items-center">
@@ -83,7 +129,7 @@
                         </div>
                       </div>
 
-                      <div class="col-md-4">
+                      <div class="col-md-4" hidden>
                         <a title="Confirmation Appointment" data-toggle="modal" data-target="#viewAptModal">
                           <div class="card comp-card">
                             <div class="card-body">
